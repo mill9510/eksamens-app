da06823dab45541802258d79acb9c08691758ba9
marv0659 <marv0659@stud.kea.dk>
2023-11-28 14:30:41 +0100

    Cleaned up project

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+import React from "react";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>Hello World</h1>
+    </main>
+  );
+}
