93d4ed443e5cec5022e669ec50b85ea3ddbb4278
kevkev14 <kevi5598@stud.kea.dk>
2023-12-11 21:40:13 +0100

    footer

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,43 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <>
+      <div class="footer">
+        <div class="footercontent">
+          <div class="box1">
+            <h1>
+              <i class="fa-solid fa-location-dot fa-sm"></i> NEWS
+            </h1>
+            <hr />
+            <br />
+            <p>CONTACT: (indsæt noget)</p>
+          </div>
+
+          <div class="box2">
+            <h1>
+              <i class="fa-solid fa-phone fa-sm"></i> ABOUT US
+            </h1>
+            <hr />
+            <br />
+            <p>LOCATION: (indsæt noget)</p>
+            <br />
+            <p>SoMe SoME</p>
+          </div>
+          <div class="box3">
+            <h1>
+              <i class="fa-solid fa-envelope fa-sm"></i> FAQ
+            </h1>
+            <hr />
+            <br />
+            <p>TIME: (indsæt noget)</p>
+          </div>
+        </div>
+        <br />
+        <hr />
+      </div>
+    </>
+  );
+}
+
+export default Footer;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,12 +2,14 @@ import styles from "./page.module.css";
 import Navbar from "./components/Navbar";
 import Forside from "./components/Forside";
 import React from "react";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Navbar />
       <Forside />
+      <Footer />
     </main>
   );
 }
