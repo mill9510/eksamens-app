2a6667133c30a01b69c719c5a4c02dd1d2a98c4f
marv0659 <marv0659@stud.kea.dk>
2023-12-17 00:36:36 +0100

    Update Footer and Galleri components

--- src/app/components/Galleri.jsx
+++ src/app/components/Galleri.jsx
@@ -6,42 +6,42 @@ function Galleri() {
     <>
       <div class="container">
         <div class="pic1">
-          <Image src="/imgs/placeholder.jpg" alt="" width={300} height={400} />
+          <Image src="/imgs/placeholder1.jpg" alt="" width={300} height={400} />
         </div>
         <div class="pic2">
           {" "}
-          <Image src="/imgs/placeholder.jpg" alt="" width={300} height={400} />
+          <Image src="/imgs/placeholder1.jpg" alt="" width={300} height={400} />
         </div>
         <div class="pic3">
           {" "}
-          <Image src="/imgs/placeholder.jpg" alt="" width={300} height={400} />
+          <Image src="/imgs/placeholder1.jpg" alt="" width={300} height={400} />
         </div>
         <div class="pic4">
           {" "}
-          <Image src="/imgs/placeholder.jpg" alt="" width={300} height={400} />
+          <Image src="/imgs/placeholder1.jpg" alt="" width={300} height={400} />
         </div>
         <div class="pic5">
           {" "}
-          <Image src="/imgs/placeholder.jpg" alt="" width={300} height={400} />
+          <Image src="/imgs/placeholder1.jpg" alt="" width={300} height={400} />
         </div>
         <div class="pic6">
           {" "}
-          <Image src="/imgs/placeholder.jpg" alt="" width={300} height={400} />
+          <Image src="/imgs/placeholder1.jpg" alt="" width={300} height={400} />
         </div>
         <div class="nocontent2"></div>
         <div class="nocontent1"></div>
         <div class="pic7">
           {" "}
-          <Image src="/imgs/placeholder.jpg" alt="" width={300} height={400} />
+          <Image src="/imgs/placeholder1.jpg" alt="" width={300} height={400} />
         </div>
         <div class="nocontent3"></div>
         <div class="pic8">
           {" "}
-          <Image src="/imgs/placeholder.jpg" alt="" width={300} height={400} />
+          <Image src="/imgs/placeholder1.jpg" alt="" width={300} height={400} />
         </div>
         <div class="pic9">
           {" "}
-          <Image src="/imgs/placeholder.jpg" alt="" width={300} height={400} />
+          <Image src="/imgs/placeholder1.jpg" alt="" width={300} height={400} />
         </div>
       </div>
     </>
